8e64e91b1758a9284f23ef873119ab19636859b7
br0067149533@enel.com <jbmarreiro@ayesa.com>
2025-03-23 22:41:12 -0300

    chore: initial setup backend

---
+++ index.js
@@ -0,0 +1,15 @@
+const { server, restify, config } = require("./server/server");
+
+server.get(
+  "/*",
+  restify.plugins.serveStatic({
+    directory: "./client",
+    default: "index.html",
+  })
+);
+
+server.listen(config.port, () => {
+  console.log(
+    `AMBIENTE: ${config.ambiente} URL: ${config.url} PORTA: ${config.port}`
+  );
+});
